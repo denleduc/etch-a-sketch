27f1f198625a5ad071c20acc06cf72c9ffadb404
Denis <denis.leduc@protonmail.com>
2021-11-05 21:42:55 +0200

    You can now draw on the grid Yay! Seriously though, you can clear the grid by typing clearGrid() in the console and you can create a new grid with the wanted size by using createGrid(size)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,37 @@
 const gridContainer = document.querySelector('.grid-container');
 let gridSize = 64;
+let gridCreated;
+let pixels = undefined;
 
-for (i=0;i< gridSize * gridSize;i++) {
-  let divSquare = gridContainer.appendChild(document.createElement('div'));
-  divSquare.classList.add('black');
-  divSquare.classList.add('pixel');
+createGrid(gridSize);
+
+function clearGrid() {
+  pixels.forEach(pixel => {
+    pixel.classList.remove('black');
+  })
 }
 
-gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+function createGrid(gridSize) {
+  if (gridCreated) {
+    while (gridContainer.firstChild) {
+      gridContainer.removeChild(gridContainer.firstChild);
+    }
+    gridCreated = false;
+  }
+
+  for (i=0;i< gridSize * gridSize;i++) {
+    let divSquare = gridContainer.appendChild(document.createElement('div'));
+    divSquare.classList.add('pixel');
+  }
+
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  pixels = document.querySelectorAll('.pixel');
+  pixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', () => {
+      pixel.classList.add('black');
+    });
+  });
+  gridCreated = true;
+}
